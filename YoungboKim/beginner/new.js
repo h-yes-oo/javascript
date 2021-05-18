b8571301b80f2d17c2c04924463b6c5f142e4916
h-yes-oo <hyesoo5115@naver.com>
2021-05-18 09:33:33 +0900

    new keyword with number object

---
+++ YoungboKim/beginner/new.js
@@ -0,0 +1,8 @@
+var obj = new Number();
+console.log(typeof obj); //object
+
+var obj1 = new Number("123");
+console.log(obj1.valueOf()); //123
+
+var obj2 = new Number("456");
+console.log(obj2.valueOf()); //456
