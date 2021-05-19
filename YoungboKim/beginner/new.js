6afee19deffa8d361df8c54a59d61f3e8a114ed2
h-yes-oo <hyesoo5115@naver.com>
2021-05-19 09:33:33 +0900

    typeof Number object and valueOf()

--- YoungboKim/beginner/new.js
+++ YoungboKim/beginner/new.js
@@ -6,3 +6,12 @@ console.log(obj1.valueOf()); //123
 
 var obj2 = new Number("456");
 console.log(obj2.valueOf()); //456
+
+var obj3 = 123;
+var obj4 = obj1 + 100;
+console.log(obj1); //[Number: 123]
+console.log(obj3); //123
+console.log(obj4); //223
+console.log(typeof obj1); //object
+console.log(typeof obj3); //number
+console.log(typeof obj4); //number
