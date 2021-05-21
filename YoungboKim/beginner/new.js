f53d7677c5903610c940b017f6022756944e68c3
h-yes-oo <hyesoo5115@naver.com>
2021-05-21 09:33:33 +0900

    number toExponential() and toFixed()

--- YoungboKim/beginner/new.js
+++ YoungboKim/beginner/new.js
@@ -6,24 +6,3 @@ console.log(obj1.valueOf()); //123
 
 var obj2 = new Number("456");
 console.log(obj2.valueOf()); //456
-
-var obj3 = 123;
-var obj4 = obj1 + 100;
-console.log(obj1); //[Number: 123]
-console.log(obj3); //123
-console.log(obj4); //223
-console.log(typeof obj1); //object
-console.log(typeof obj3); //number
-console.log(typeof obj4); //number
-
-var value = 20;
-console.log(20 === value.toString()); //false
-//16진수로 번환
-console.log(value.toString(16)); //14
-
-console.log(20..toString());
-
-var toLocale = 1234.56;
-console.log(toLocale.toLocaleString());
-console.log(toLocale.toLocaleString('de-DE'));
-console.log(toLocale.toLocaleString('zh-Hans-CN-u-nu-hanidec'));
--- YoungboKim/beginner/number.js
+++ YoungboKim/beginner/number.js
@@ -8,7 +8,42 @@ console.log(Number(undefined)); //NaN
 
 console.log(Number.MAX_VALUE);
 console.log(Number.MIN_VALUE);
-  console.log(Number.NaN);
-    console.log(Number.POSITIVE_INFINITY);
-      console.log(Number.NEGATIVE_INFINITY);
+console.log(Number.NaN);
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NEGATIVE_INFINITY);
 
+var obj1 = new Number("123");
+console.log(obj1.valueOf()); //123
+
+var obj3 = 123;
+var obj4 = obj1 + 100;
+console.log(obj1); //[Number: 123]
+console.log(obj3); //123
+console.log(obj4); //223
+console.log(typeof obj1); //object
+console.log(typeof obj3); //number
+console.log(typeof obj4); //number
+
+var value = 20;
+console.log(20 === value.toString()); //false
+//16진수로 번환
+console.log(value.toString(16)); //14
+
+console.log(20..toString());
+
+var toLocale = 1234.56;
+console.log(toLocale.toLocaleString());
+console.log(toLocale.toLocaleString('de-DE'));
+console.log(toLocale.toLocaleString('zh-Hans-CN-u-nu-hanidec'));
+
+var val = 123;
+console.log(val.toExponential()); //1.23e+2
+
+var val2 = 123456;
+console.log(val2.toExponential()); //1.23456e+5
+console.log(val2.toExponential(3)); //1.235e+5
+
+var val3 = 1234.567;
+console.log(val3.toFixed()); //1235
+console.log(val3.toFixed(2)); //1234.57
+console.log(val3.toFixed(5)); //1234.56700
