5face3742e011c326a0f0b696c1e19a6da6cb716
h-yes-oo <hyesoo5115@naver.com>
2021-05-25 09:33:33 +0900

    string object and length property

---
+++ YoungboKim/beginner/string.js
@@ -0,0 +1,15 @@
+var haha = "hi" + " nice to meet " + "you";
+console.log(haha);
+
+var value = String(123);
+console.log(value.length);
+console.log(value);
+console.log(typeof value);
+
+var obj = new String(123);
+console.log(typeof obj);
+console.log(obj.valueOf());
+
+var value = "ABC";
+for(var i = 0; i < value.length; i++)
+	console.log(value[i]);
