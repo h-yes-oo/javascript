bf0711c87188112c118c95e71dec7d2152f4bc7d
h-yes-oo <hyesoo5115@naver.com>
2021-06-02 09:33:33 +0900

    object object

---
+++ YoungboKim/beginner/dom.js
@@ -0,0 +1,8 @@
+// var node = document.querySelector("div");
+// console.log(node.nodeName);
+
+var abc = new Object(); //instance
+var obj = {}; //object
+
+console.log(typeof abc);
+console.log(typeof obj);
