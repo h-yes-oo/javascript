ab03525902bc7b172a1ce81d7c0d8e902fe6f031
h-yes-oo <hyesoo5115@naver.com>
2021-06-09 09:33:33 +0900

    hasOwnProperty() and propertyIsEnumberable()

---
+++ YoungboKim/beginner/objectProperty.js
@@ -0,0 +1,17 @@
+var obj = { coco: "so cute" };
+var own = obj.hasOwnProperty("coco");
+var ownInherited = obj.hasOwnProperty("hasOwnProperty");
+console.log(own);
+console.log(ownInherited);
+
+var obj = {sports: "축구"};
+console.log(obj.propertyIsEnumerable("sports"));
+
+Object.defineProperty(obj, "sports", {
+  enumerable: false
+});
+console.log(obj.propertyIsEnumerable("sports"));
+
+for(var name in obj){
+  console.log(name);
+}
