570e0a71561d02922b04d24092aeaecdb40048d6
h-yes-oo <hyesoo5115@naver.com>
2021-06-15 10:19:19 +0900

    function object and length

--- YoungboKim/beginner/functionObject.js
+++ YoungboKim/beginner/functionObject.js
@@ -3,3 +3,17 @@ console.log(obj("hihi"));
 
 var ftnWithTwo = new Function("one","two","return one + two;");
 console.log(ftnWithTwo(3,100));
+
+function getBook(title){
+  return title;
+}
+
+var result = getBook("js book");
+console.log(result);
+
+function add(one, two) {
+  return one + two;
+}
+
+console.log(add(1));
+console.log(add.length);
