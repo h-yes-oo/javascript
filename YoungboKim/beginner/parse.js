2ad04736f9047e6eddd407b6785316218c215acd
h-yes-oo <hyesoo5115@naver.com>
2021-07-02 09:31:31 +0900

    parseInt, parseFloat

---
+++ YoungboKim/beginner/parse.js
@@ -0,0 +1,8 @@
+console.log(parseInt("123px")); //123
+console.log(parseInt("0012")); //12
+console.log(parseInt("1 2 3 4")); //1
+
+console.log(parseFloat("1.2e3")); //1200
+console.log(parseFloat("1 2 3 4")); //1
+console.log(parseFloat("12 34")); //12
+console.log(parseFloat()); //NaN
